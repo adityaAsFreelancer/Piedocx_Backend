bd47eade44c94d227077031583c08f70bb33b7ee
adityaAsFreelancer <rajvanshi1234yd@GMAIL.COM>
2025-08-22 13:17:42 +0530

    addedd successfully

--- src/Entities/Test.entity.ts
+++ src/Entities/Test.entity.ts
@@ -29,6 +29,9 @@ export class Test extends BaseEntity {
   @Column({ name: 'time', type: 'int', nullable: true })
   time!: number;
 
+  @Column({ name: 'isActive', type: 'boolean', default: false })
+  isActive!: boolean;
+
   @CreateDateColumn({ name: 'createdAt', type: 'timestamptz' })
   createdAt!: Date;
 
